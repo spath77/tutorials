8978fa82082d0911011acc6493ed8a5c37c98a1c
Konstantinos <spatharis77@gmail.com>
2024-02-21 23:47:01 +0200

    modified:   randomMovie.js

--- randomMovie.js
+++ randomMovie.js
@@ -1,7 +1,7 @@
-const chooseMovie = (choices) => {
-  return choices[Math.floor(Math.random() * choices.length)];
-};
+const chooseMovie = (choices) =>
+  choices[Math.floor(Math.random() * choices.length)];
+
 const displayMovieChoice = () => {
   const movieChoice = chooseMovie(movies);
-  document.getElementById("movie-choice").innerHTML = movieChoice.title;
+  document.getElementById("movie-choice").innerHTML = ` ${movieChoice.title}`;
 };
