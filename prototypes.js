07768079b9d1c003b7db6cf75fbaabaee1981b15
Konstantinos <spatharis77@gmail.com>
2024-02-22 22:55:26 +0200

    modified:   prototypes.js

--- prototypes.js
+++ prototypes.js
@@ -11,3 +11,16 @@ function makeUnorderedList(array = []) {
   let listItems = array.map((element) => `<li>${element}</li>`);
   return `<ul>${listItems.join("")}</ul>`;
 }
+
+const myTruck = {
+  speed: 0,
+};
+
+const myCar = {
+  speed: 0,
+};
+
+function drive(vehicle, speedLimit) {
+  vehicle.speed = speedLimit;
+  console.log(`Driving at ${vehicle.speed}mph.`);
+}
